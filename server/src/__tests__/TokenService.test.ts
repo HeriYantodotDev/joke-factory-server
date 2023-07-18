804c2630684570db4879f20e150aecaaad01b72c
HeriYantodotDev <yanto.heri@outlook.com>
2023-07-18 13:48:56 +0700

    adding clean up token function every one hour

---
+++ server/src/__tests__/TokenService.test.ts
@@ -0,0 +1,45 @@
+import { sequelize } from '../config/database';
+import { Auth, User } from '../models';
+import { 
+  AuthHelperModel,
+  UserHelperModel
+} from '../models';
+
+
+beforeAll( async () => {
+  await sequelize.sync();
+});
+
+beforeEach( async() => {
+  await User.destroy({truncate: true});
+  await Auth.destroy({truncate: true});
+});
+
+
+describe('Scheduled Token Clean Up', () => {
+  test('clears the expired token with scheduled task', async () => {
+    jest.useFakeTimers();
+    const savedUser = await UserHelperModel.addMultipleNewUsers(1);
+    const token = 'test-token';
+    const eightDaysAgo = AuthHelperModel.maxAgeTokenByDay(8);
+    await Auth.create({
+      token,
+      userID: savedUser[0].id,
+      lastUsedAt: eightDaysAgo,
+    });
+
+
+    AuthHelperModel.scheduleCleanUp();
+
+    jest.advanceTimersByTime((60*60*1000) + 5000 );
+    const tokenInDB = await Auth.findOne({
+      where: {token: token},
+    });
+
+    expect(tokenInDB).toBeNull();
+
+
+  });
+});
+
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import { app } from './app';
 import { sequelize } from './config/database';
-import { UserHelperModel } from './models';
+import { UserHelperModel, AuthHelperModel } from './models';
 
 const PORT = 3000;
 
 sequelize.sync({force: true}).then(async () => {
   await UserHelperModel.addMultipleNewUsers(25);
 });
+
+AuthHelperModel.scheduleCleanUp();
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
