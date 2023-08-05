e1a5fa26f758fe7ac2b45075a706ec59c351ed58
Heri yanto <yanto.heri@outlook.com>
2023-08-05 14:24:18 +0700

    file type validation

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config.InitialOptions = {
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
   ],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 };
 
 export default config;
