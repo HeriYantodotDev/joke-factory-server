f9de9cb89d3a1a4e0232bc4962f496b2f2a4b31f
Heri yanto <yanto.heri@outlook.com>
2023-08-09 17:53:50 +0700

    configure a little bit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { sequelize } from './config/database';
 import { AuthHelperModel } from './models';
 import { logger } from './utils';
 
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
 sequelize.sync();
 
